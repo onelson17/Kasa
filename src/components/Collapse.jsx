49c91ef4599770d528d58ea62225e09d5502de2c
onelson <thebantonn@gmail.com>
2026-01-27 02:14:32 +0100

    intégration de la page à propos + composant Collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -2,24 +2,25 @@ import '../css/Collapse.css'
 import Arrow from '../assets/Arrow.png'
 import { useState } from 'react'
 
-function Collapse({ title, texte }) {
+function Collapse({ title, content }) {
 
     const [isOpen, setIsOpen] = useState(false)
     const iconCollapse = () => {
         setIsOpen(!isOpen)
     }
 
-console.log(iconCollapse)
     return (
         <>
-            <article  className='collapse'>
-                <h2>{title}</h2>
-                <img onClick={iconCollapse} src={Arrow} alt="Ouvrir" />
-            </article>
+            <section >
+                <article  className='collapse'>
+                    <h2>{title}</h2>
+                    <img onClick={iconCollapse} src={Arrow} alt="Ouvrir" className={` arrow ${isOpen ? 'opened' : 'closed'} `}/>
+                </article>
 
-            <div className='collapse--content'>
-               {texte}
-            </div>
+                <div className={` collapse--content ${isOpen ? 'content--open' : 'content--close'} `}>
+                {content}
+                </div>
+            </section>
         
         </>
     )
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,16 +12,16 @@ function About() {
             </div>
             <section className="about-contain">
                 <div>
-                    <Collapse title="Fiabilité" texte="hello world"/>
+                    <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
                 </div>
                 <div>
-                    <Collapse title="Respect" texte="hello world"/>
+                    <Collapse title="Respect" content="La bienveillance fait partie des valeurs de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme."/>
                 </div>
                 <div>
-                    <Collapse title="Service" texte="hello world" />
+                    <Collapse title="Service" content="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interactions, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect de bienveillance." />
                 </div>
                 <div>
-                    <Collapse title="Sécurité" texte="hello world" />
+                    <Collapse title="Sécurité" content="La sécurité est la priorité des Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
                 </div>
             </section>
             
