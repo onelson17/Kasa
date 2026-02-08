3d109542a15307759c47ed50a095f6908d6b3e21
onelson <thebantonn@gmail.com>
2026-02-08 14:00:20 +0100

    responsive tablette

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,13 +4,13 @@ import '../css/Error.css'
 
 function Error() {
     return (
-        <main className="error-page">
+    <div className="error-page">
         <h1 className="error--number">404</h1>
         <p className="error--text">Oups! La page que vous demandez n'existe pas.</p>
         <Link to="/" className="error--redirection">
             Retourner sur la page d'accueil
         </Link>
-    </main>
+    </div>
     )
 
 }
